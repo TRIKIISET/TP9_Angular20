48c10fc1807c574b02498d9de82486b46d2c9640
TRIKIISET <56267315+TRIKIISET@users.noreply.github.com>
2025-11-05 09:14:37 +0100

    Update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { GameAdd } from './application/components/game-add/game-add';
 import { EmployeAdd } from './exercice/components/employe-add/employe-add';
-import { EmployeList } from './exercice/components/employe-list/employe-list';
 
 @Component({
   selector: 'app-root',
-  imports: [ GameAdd, EmployeAdd, EmployeList],
+  imports: [ GameAdd, EmployeAdd],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('TP8');
 }
+
